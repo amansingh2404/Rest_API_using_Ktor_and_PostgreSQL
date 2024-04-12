4308168bc4403ede4fafbc990a775a05bd712b67
Aman <amansingh050502@gmail.com>
2024-04-12 12:08:27 +0530

    "Initial Commit

--- src/main/kotlin/route/Route.kt
+++ src/main/kotlin/route/Route.kt
@@ -23,7 +23,7 @@ class CreateStudent
 fun Route.student(
     db: StudentRepository
 ) {
-    post("/student") {
+    post("/student/insert") {
         val parameter = call.receive<Parameters>()
         val name = parameter["name"] ?: return@post call.respondText(
             "Missing Field",
@@ -42,6 +42,53 @@ fun Route.student(
 
         }catch (e: Throwable){
             call.respondText("${e.message}")
+        }
+    }
+    get("/student"){
+        try {
+            val allStudent = db.getAllStudent()
+            if(allStudent.isNotEmpty()){
+                call.respond(allStudent)
+            }else{
+                call.respond("No data found")
+            }
+        }catch (e: Throwable){
+            application.log.error("Failed to get user", e)
+            call.respond(HttpStatusCode.BadRequest, "Problem in finding user")
+        }
+    }
+    get("student/{id}"){
+        val parameter = call.parameters["id"]
+        try{
+            val user = parameter?.toInt()?.let { it1 -> db.getStudentById(it1) } ?: return@get call.respondText(
+                "Invalid Id",
+                status = HttpStatusCode.BadRequest
+            )
+            user.userId.let{
+                call.respond(status = HttpStatusCode.OK, user)
+            }
+        }catch (e: Throwable){
+            application.log.error("Failed to register User", e)
+            call.respond(HttpStatusCode.BadRequest, "Problems creating user")
+        }
+    }
+    delete("student/{id}"){
+        val parameter = call.parameters["id"]
+        try{
+            val user = parameter?.toInt()?.let { it1-> db.deleteById(it1) } ?: return@delete call.respondText(
+                "No id found...",
+                status = HttpStatusCode.BadRequest
+            )
+            if(user == 1){
+                call.respondText("Deleted Successfully", status = HttpStatusCode.OK)
+
+            }else{
+                call.respondText("Id not found ", status = HttpStatusCode.BadRequest)
+
+            }
+        }catch (e: Throwable){
+            application.log.error("Failed to register user", e)
+            call.respond(HttpStatusCode.BadRequest, "Problem creating user")
         }
     }
 }
